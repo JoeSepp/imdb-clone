e1893164a1edfde65e37c31520034c58e4ebef78
Pepik <pepa.maurenc@seznam.cz>
2026-04-15 13:19:49 +0200

    Changed licenced photos to placeholders on the landing page

--- src/Components/Hero/HeroContent.jsx
+++ src/Components/Hero/HeroContent.jsx
@@ -4,10 +4,12 @@ function HeroContent(props) {
     const { img, poster, label, sublabel, likes, cheers, lengthSeconds } = props.data;
     return (
 
+        //src={img} nelicencované fotky
+
         <div className="current-hero-label">
-            <img className="hero-image" src={img} />
+            <img className="hero-image" src="src\Images\placeholder.png" /> 
             <div className="current-container">
-                <img className="hero-poster" src={poster} />
+                <img className="hero-poster" src="src\Images\placeholder.png" />
                 <div className="current-hero-label-info">
                     <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" className="ipc-icon ipc-icon--play-circle-outline-large-inline sc-809ef538-5 gLhmTs slide-caption-play-icon" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path></svg>
                     <span className="label-text-area">
