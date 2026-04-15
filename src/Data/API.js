3f4378687bc094b3166eeaab7583525dd6cbc527
Pepik <pepa.maurenc@seznam.cz>
2026-04-15 14:35:17 +0200

    creating env and gitignore file

--- src/Data/API.js
+++ src/Data/API.js
@@ -1,8 +1,13 @@
+
+
+const apiKey = import.meta.env.VITE_API_KEY;
+
+
 const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzMzMTUwMzg2NDYxODMzMzEwNTc5ZmUwOTNlMDMyNiIsIm5iZiI6MTc1NTA2Nzg5Ny44OTUsInN1YiI6IjY4OWMzNWY5MzdlYjk3ZWM3NGI3MDk4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HqDk6K8luPmOh1yruRo-hz5wZk_zEEBeKQBZoD1Ikgc'
+        Authorization: `Bearer ${apiKey}`
     }
 };
 
