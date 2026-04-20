405e343871b7525573281d82c04ef6842c93221a
Pepik <pepa.maurenc@seznam.cz>
2026-04-20 11:24:45 +0200

    Updated the main hero slider to whenever main slider changes the side slider does same

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,23 +1,84 @@
 import newsData from "../../Data/newsData.js"
 import NewVideos from "./NewVideos.jsx"
 import "../../Styles/Hero/Hero.css"
-import { useState } from "react"
-import MainSlider from "./Sliders/MainSlider.jsx"
-import SideSlider from "./Sliders/SideSlider.jsx"
+import { useState, useRef, useEffect } from "react"
+import HeroContent from "./HeroContent.jsx";
+import Slider from "react-slick"
+import NextArrow from "../Arrows/NextArrow.jsx"
+import PrevArrow from "../Arrows/PrevArrow.jsx"
+import "../../Styles/Sliders/SideSlider.css"
+import "../../Styles/Sliders/MainSlider.css"
 
-
-/* Udělat animaci pro slider jako je na stránkách pak asi pokračujem dál  */
 
 function Hero(props) {
-    const [slider, setSlider] = useState(newsData)
+    const [mainSlider, setMainSlider] = useState(newsData)
     const [queue, setQueue] = useState(newsData)
+    const [nav1, setNav1] = useState(null);
+    const [nav2, setNav2] = useState(null);
+    let sliderRef1 = useRef(null);
+    let sliderRef2 = useRef(null);
+
+    useEffect(() => {
+        setNav1(sliderRef1);
+        setNav2(sliderRef2);
+    }, [])
+
 
     return (
         <div className="hero-section">
-            <MainSlider slider={slider} setSlider={setSlider} />
-            <SideSlider queue={queue} setQueue={setQueue} />
-        </div>
+            <Slider asNavFor={nav2}
+                ref={slider => (sliderRef1 = slider)}
+                className="hero-content-slider"
+                autoplay={false}
+                nextArrow={<NextArrow />}
+                prevArrow={<PrevArrow />}
+                autoplaySpeed={5000}
+                speed={500}
+                draggable={false}
+            >
+                {mainSlider.map((slide) => {
+                    return (
+                        <div>
+                            <HeroContent data={slide} />
+                        </div>
+                    )
+                })}
+            </Slider>
+
+            <div className="hero-label-list_next">
+                <span className="up-next_label-text">Up next</span>
+                <div className="up-next-div-container">
+                    <Slider
+                        asNavFor={nav1}
+                        ref={slider => (sliderRef2 = slider)}
+                        slidesToShow={3}
+                        vertical={true}
+                        className="side-hero-slider"
+                        autoplay={false}
+                        arrows={false}
+                        draggable={false}
+                    >
+                        {queue.map((vid, keyid) => {
+                            return <NewVideos video={vid} key={keyid} />
+                        })}
+                    </Slider>
+                </div>
+                <span className="browse-trailer_text">Browse trailers &gt;</span>
+            </div>
+        </div >
     )
+
+
+
+
+    /*
+       return (
+           <div className="hero-section">
+               <MainSlider slider={slider} setSlider={setSlider} />
+               <SideSlider queue={queue} setQueue={setQueue} />
+           </div>
+       )
+           */
 }
 
 export default Hero
