7e1fc7381c06c2e4e2c04ab950eff17c78dd61d9
Pepik <pepa.maurenc@seznam.cz>
2026-04-20 12:41:33 +0200

    Covered content into div "page-content-container" and removed some unnecessary styling

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -25,28 +25,29 @@ import exploreData from "../Data/exploreData.js"
 
 
 function Main() {
-    
+
 
     return (
         <main>
-            <Hero />
-            <div className="gallery-container">
-                <span className="gallery-header">Featured today</span>
-                <FeaturedSlider data={featuredTodayData} />
+            <div className="page-content-container">
+                <Hero />
+                <div className="gallery-container">
+                    <span className="gallery-header">Featured today</span>
+                    <FeaturedSlider data={featuredTodayData} />
+                </div>
+                <PeopleSlider />
+                <div className="gallery-container">
+                    <span className="gallery-header">What to watch</span>
+                    <MovieSlider api={"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"} header="Top picks" subtext="TV shows and movies just for you" />
+                    <MovieSlider api={"https://api.themoviedb.org/3/trending/tv/day?language=en-US"} header="Top 10 on IMDb this week" ranked={true} />
+                    <MovieSlider api={"https://api.themoviedb.org/3/trending/movie/day?language=en-US"} header="Fan favorites" subtext="This week's top TV and movies" />
+                    <PopularInterests />
+                </div>
+                <div className="gallery-container">
+                    <span className="gallery-header">More to explore</span>
+                    <FeaturedSlider data={exploreData} header="Editor's pick" />
+                </div>
             </div>
-            <PeopleSlider />
-            <div className="gallery-container">
-                <span className="gallery-header">What to watch</span>
-                <MovieSlider api={"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"} header="Top picks" subtext="TV shows and movies just for you" />
-                <MovieSlider api={"https://api.themoviedb.org/3/trending/tv/day?language=en-US"} header="Top 10 on IMDb this week" ranked={true} />
-                <MovieSlider api={"https://api.themoviedb.org/3/trending/movie/day?language=en-US"} header="Fan favorites" subtext="This week's top TV and movies" />
-                <PopularInterests />
-            </div>
-            <div className="gallery-container">
-                <span className="gallery-header">More to explore</span>
-                <FeaturedSlider data={exploreData} header="Editor's pick" />
-            </div>
-
         </main>
     )
 }
