2c20d06388e6518af7fb88a9d219f8531948d259
Pepik <pepa.maurenc@seznam.cz>
2026-04-22 11:35:12 +0200

    improved hero slider/section responsivity

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -25,53 +25,58 @@ function Hero(props) {
 
 
     return (
-        <div className="hero-section" style={{ gridArea: "hero-area" }}>
-            <Slider asNavFor={nav2}
-                ref={slider => (sliderRef1 = slider)}
-                className="hero-content-slider"
-                autoplay={false}
-                nextArrow={<NextArrow />}
-                prevArrow={<PrevArrow />}
-                autoplaySpeed={5000}
-                speed={500}
-                draggable={false}
-            >
-                {mainSlider.map((slide) => {
-                    return (
-                        <HeroContent data={slide} />
-                    )
-                })}
-            </Slider>
-
-            <div className="hero-label-list_next" style={{ gridArea: "side-hero-area" }}>
-                <div className="flex-container-up-next">
-                    <div className="up-next_label-header">
-                        <span className="up-next_label-text">Up next</span>
-                    </div>
-                    <div className="up-next-div-container">
-                        <Slider
-                            asNavFor={nav1}
-                            ref={slider => (sliderRef2 = slider)}
-                            slidesToShow={3}
-                            vertical={true}
-                            className="side-hero-slider"
-                            autoplay={false}
-                            arrows={false}
-                            draggable={false}
-                        >
-                            {queue.map((vid, keyid) => {
-                                return <NewVideos video={vid} key={keyid} />
-                            })}
-                        </Slider>
-                    </div>
+        <div>
+            <div className="hero-section" style={{ gridArea: "hero-area" }}>
+                <div style={{ maxWidth: "100%", position: "relative" }}>
+                    <Slider asNavFor={nav2}
+                        ref={slider => (sliderRef1 = slider)}
+                        className="hero-content-slider"
+                        autoplay={false}
+                        nextArrow={<NextArrow />}
+                        prevArrow={<PrevArrow />}
+                        autoplaySpeed={5000}
+                        speed={500}
+                        draggable={false}
+                    >
+                        {mainSlider.map((slide) => {
+                            return (
+                                <HeroContent data={slide} />
+                            )
+                        })}
+                    </Slider>
                 </div>
-                <span className="browse-trailer_text">Browse trailers &gt;</span>
-            </div>
 
+                <div className="hero-label-list_next" style={{ gridArea: "side-hero-area" }}>
+                    <div className="flex-container-up-next">
+                        <div className="up-next_label-header">
+                            <span className="up-next_label-text">Up next</span>
+                        </div>
+                        <div className="up-next-div-container">
+                            <Slider
+                                asNavFor={nav1}
+                                ref={slider => (sliderRef2 = slider)}
+                                slidesToShow={3}
+                                vertical={true}
+                                className="side-hero-slider"
+                                autoplay={false}
+                                arrows={false}
+                                draggable={false}
+                            >
+                                {queue.map((vid, keyid) => {
+                                    return <NewVideos video={vid} key={keyid} />
+                                })}
+                            </Slider>
+                        </div>
+                    </div>
+                    <span className="browse-trailer_text">Browse trailers &gt;</span>
+                </div>
+
+
+            </div >
             <div className="page-grid-divider" style={{ gridArea: "bottom-border-area" }}>
                 <div className="grid-bottom-border" ></div>
             </div>
-        </div >
+        </div>
     )
 
 
