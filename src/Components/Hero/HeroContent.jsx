13599c0154dc7b878fa1a8ee0de531b98541c7ef
Pepik <pepa.maurenc@seznam.cz>
2026-04-22 11:55:34 +0200

    fixed minor issues

--- src/Components/Hero/HeroContent.jsx
+++ src/Components/Hero/HeroContent.jsx
@@ -24,7 +24,7 @@ function HeroContent(props) {
                     <div className="hero_overlay-content">
                         <figcaption className="hero_overlay-figcaption">
                             <div className="hero_svg-play-icon">
-                                <svg width="72" height="72" xmlns="http://www.w3.org/2000/svg" class="ipc-icon ipc-icon--play-circle-outline-large-inline sc-c7eb995d-5 fGMoBC slide-caption-play-icon" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path></svg>
+                                <svg width="72" height="72" xmlns="http://www.w3.org/2000/svg" className="ipc-icon ipc-icon--play-circle-outline-large-inline sc-c7eb995d-5 fGMoBC slide-caption-play-icon" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path><path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path></svg>
                                 <span className="hero_video-length bp600">{Math.floor(lengthSeconds / 60) + ":" + (lengthSeconds % 60)}</span>
                             </div>
                             <div className="hero_overlay-figcaption-content">
