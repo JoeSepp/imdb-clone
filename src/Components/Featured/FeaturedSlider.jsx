a09ee170bb3aa8021be2a71fb6b19fb66fc503d4
Pepik <pepa.maurenc@seznam.cz>
2026-04-24 10:53:17 +0200

    improved main site responsivnes + paddings and margins on each element matching morethe original site

--- src/Components/Featured/FeaturedSlider.jsx
+++ src/Components/Featured/FeaturedSlider.jsx
@@ -19,19 +19,10 @@ function FeaturedSlider(props) {
         slidesToScroll: 1,
         nextArrow: <NextArrow />,
         prevArrow: <PrevArrow />,
-        mobileFirst: true,
-        responsive: [
-            {
-                breakpoint: 700,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            }
-        ]
+        mobileFirst: true
     }
 
-       if (window.innerWidth <= 600) {
+    if (window.innerWidth <= 600) {
         settings = {
             ...settings,
             slidesToShow: 1,
@@ -41,14 +32,20 @@ function FeaturedSlider(props) {
 
     return (
         <>
-            <span className="container-header">
-                {props.header} {props.header && <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" className="ipc-icon ipc-icon--chevron-right-inline ipc-icon--inline ipc-title-link ipc-title-link-chevron" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M5.622.631A2.153 2.153 0 0 0 5 2.147c0 .568.224 1.113.622 1.515l8.249 8.34-8.25 8.34a2.16 2.16 0 0 0-.548 2.07c.196.74.768 1.317 1.499 1.515a2.104 2.104 0 0 0 2.048-.555l9.758-9.866a2.153 2.153 0 0 0 0-3.03L8.62.61C7.812-.207 6.45-.207 5.622.63z"></path></svg>}
-            </span>
-            <Slider {...settings}>
-                {featuredData.map((data, index) => {
-                    return (<div key={index}><FeaturedToday featured={data} /></div>)
-                })}
-            </Slider>
+            <div>
+                <span className="container-header"></span>
+                <Slider {...settings}>
+                    {featuredData.map((data, index) => {
+                        return (<div key={index}><FeaturedToday featured={data} /></div>)
+                    })}
+                </Slider>
+                <div className="featured-slider-bp1080">
+                    {featuredData.map((data, index) => {
+                        return (<div key={index}><FeaturedToday featured={data} /></div>)
+                    })}
+                </div>
+            </div>
+
         </>
     )
 }
